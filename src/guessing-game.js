3133f6b74eb2f51f143fe421c3d88487a6037b8a
aabelov2703 <alex.belov.2703@gmail.com>
2021-08-17 21:00:29 +0300

    feat: guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+  
     guess() {
-
+      console.log(`${this.min} ${this.max} ${Math.ceil((this.min + this.max) / 2)}`);
+      return Math.ceil((this.min + this.max) / 2);
     }
-
+  
     lower() {
-
+        this.setRange(this.min, Math.ceil((this.min + this.max) / 2));
+        console.log(`lower new range = ${this.min} ${this.max}`)
     }
-
+  
     greater() {
-
+        this.setRange(Math.ceil((this.min + this.max) / 2), this.max);
+        console.log(`greater new range = ${this.min} ${this.max}`)
     }
 }
 
